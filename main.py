be992d598dad6c18810b9ecf840a86c87a919718
Zach Thomas <zacherythomas12@gmail.com>
2017-05-31 19:20:03 -0400

    Added api call for posting to twitter

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ __author__ = 'Zachery Thomas'
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+from tweepy import API
 from haiku import haiku
 from config import *
 import unicodedata
 import threading
 import time
+import re
 
+global api
 
 def no_unicode(data):
     return unicodedata.normalize('NFKD' , data).encode('ascii', 'ignore')
@@ -22,14 +25,14 @@ class WorkerThread (threading.Thread):
         self.author = author
 
     def run(self):
+        if not re.match("""^[A-Za-z0-9 _.,!"'/$]*$""", self.text): exit()
+
         # Links aren't cool
         if 'http' in self.text: exit()
 
-        # Don't want @'s
-        if '@' in self.text: exit()
-
         result = haiku(self.text)
         if result:
+            api.update_status(result)
             print result
             print '  -@%s' % self.author
             print
@@ -50,6 +53,8 @@ auth.set_access_token(access_token, access_token_secret)
 
 hsl = HaikuStreamListener()
 stream = Stream(auth, hsl, timeout=None)
+
+api = API(auth)
 
 while 1:
     try:
